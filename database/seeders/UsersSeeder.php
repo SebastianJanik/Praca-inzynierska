0720165075f655d4852435ee67e215c2bbcdcd59
sjanik <sebastian.janik@devcomm.pl>
2022-01-20 15:57:22 +0100

    New seeders

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -21,6 +21,10 @@ class UsersSeeder extends Seeder
     private $towns;
     private $streets;
     private $numberOfPlayers;
+    private $numberOfReferees;
+    private $numberOfCoaches;
+    private $numberOfUsers;
+    private $today;
 
     public function __construct()
     {
@@ -29,14 +33,26 @@ class UsersSeeder extends Seeder
         $this->firstnames = ["Piotr", "Krzysztof", "Andrzej", "Tomasz", "Jan", "Paweł", "Michał", "Marcin", "Stanisław", "Jakub", "Adam", "Marek", "Grzegorz", "Mateusz", "Wojciech", "Mariusz", "Dariusz", "Zbigniew", "Jerzy", "Maciej", "Rafał", "Robert", "Józef", "Kamil", "Jacek", "Tadeusz", "Dawid", "Ryszard", "Szymon", "Kacper", "Janusz", "Bartosz", "Jarosław", "Mirosław", "Sławomir", "Henryk", "Artur", "Sebastian", "Damian", "Patryk", "Kazimierz", "Przemysław", "Daniel", "Karol", "Roman", "Marian", "Wiesław", "Antoni", "Filip", "Adrian", "Arkadiusz", "Aleksander", "Dominik", "Bartłomiej", "Leszek", "Franciszek", "Waldemar", "Mikołaj", "Zdzisław", "Krystian", "Radosław", "Wiktor", "Bogdan", "Edward", "Mieczysław", "Konrad", "Władysław", "Hubert", "Czesław", "Igor", "Eugeniusz", "Oskar", "Stefan", "Bogusław", "Zygmunt", "Ireneusz", "Marcel", "Witold", "Maksymilian", "Sylwester", "Miłosz", "Włodzimierz", "Zenon", "Alan", "Oliwier", "Cezary", "Nikodem", "Norbert", "Leon", "Gabriel", "Julian", "Błażej", "Oleksandr", "Fabian", "Bronisław", "Ignacy", "Emil", "Eryk", "Wacław", "Tymoteusz", "Lech", "Bolesław", "Tymon", "Bernard", "Edmund", "Serhii", "Volodymyr", "Andrii", "Remigiusz", "Ksawery", "Natan", "Lucjan", "Olaf", "Romuald", "Borys", "Kajetan", "Szczepan", "Albert", "Seweryn", "Gracjan", "Alfred", "Kuba", "Dmytro", "Tobiasz", "Ivan", "Ludwik", "Joachim", "Mykola", "Lesław", "Bogumił", "Vasyl", "Gerard", "Vitalii", "Ernest", "Maksym", "Ihor", "Bruno", "Feliks", "Kornel", "Olivier", "Jędrzej", "Yurii", "Alojzy", "Viktor", "Oleh", "Alexander", "Alex", "Mykhailo", "Bohdan", "Juliusz", "Leonard", "Klaudiusz", "David", "Benedykt", "Aleks", "Dorian", "Rajmund", "Teodor", "Cyprian", "Martin", "Oliver", "Oleksii", "Vladyslav", "Rudolf", "Nataniel", "Konstanty", "Ruslan", "Denis", "Hieronim", "Brajan", "Gustaw", "Michael", "Wincenty", "Samuel", "Zygfryd", "Marceli", "Pavlo", "Florian", "Erwin", "Mieszko", "Kevin", "Fryderyk", "Ariel", "Denys", "Anatolii", "Iwo", "Vadym", "Milan", "Artem", "Aleksy", "Petro", "Beniamin", "Roland", "Sergiusz", "Walenty", "Yaroslav", "Kordian", "Adolf"];
         $this->streets = ['1 Maja', 'Bazylianów', 'Cicha', 'Długa', 'Fabryczna', 'Groszkowa', 'Hugo Kołłątaja', 'Ignacego Mościckiego', 'Jana Matejki', 'Krochmalna', 'Leopolda Staffa', 'Marii Curie-Skłodowskiej', 'Niepodległości', 'Okopowa', 'Plażowa', 'Robotnicza', 'Siewna', 'Tadeusza Kościuszki', 'Westerplatte', 'Zamojska'];
         $this->towns = ['Bychawa', 'Kraśnik', 'Świdnik', 'Lublin', 'Zamość', 'Chełm', 'Krasnystaw', 'Krasnobród', 'Łęczna', 'Końskowola', 'Piotrowice', 'Łóków', 'Lubartów', 'Włodawa', 'Piszczac', 'Zakrzówek', 'Wilkołaz', 'Puławy', 'Nałęczów'];
+        $this->today = date("Y-m-d H:i:s");
         $this->numberOfPlayers = 115;
+        $this->numberOfReferees = 5;
+        $this->numberOfCoaches = 23;
+        $this->numberOfUsers = 5;
     }
 
     public function run()
     {
-        $today = date("Y-m-d H:i:s");
-        $rolePlayer = Role::findByName('player');
-        for($i=0; $i < $this->numberOfPlayers; $i++) {
+        $this->players();
+        $this->admin();
+        $this->coaches();
+        $this->referees();
+        $this->users();
+    }
+
+    public function players()
+    {
+        $role = Role::findByName('player');
+        for($i=1; $i <= $this->numberOfPlayers; $i++) {
             DB::table($this->name)->insert(
                 [
                     'name' => $this->firstnames[array_rand($this->firstnames)],
@@ -48,14 +64,124 @@ class UsersSeeder extends Seeder
                     'town' => $this->towns[array_rand($this->towns)],
                     'email' => 'player'.$i.'@player.com',
                     'password' => Hash::make('admin123'),
-                    'created_at' => $today
+                    'created_at' => $this->today
                 ]
             );
             DB::table('model_has_roles')->insert(
                 [
-                    'role_id' => $rolePlayer->id,
+                    'role_id' => $role->id,
                     'model_type' => 'App\Models\user',
                     'model_id' => $i+1
+                ]
+            );
+        }
+    }
+
+    public function admin()
+    {
+        $role = Role::findByName('admin');
+        $id = DB::table($this->name)->insertGetId(
+            [
+                'name' => 'admin',
+                'surname' => 'admin',
+                'date_birth' => rand(1989, 2005) . '-' . rand(1, 12) . '-' . rand(1, 28),
+                'street' => $this->streets[array_rand($this->streets)],
+                'house_number' => rand(1, 250),
+                'postal_code' => rand(10, 99) . '-' . rand(100, 999),
+                'town' => $this->towns[array_rand($this->towns)],
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('admin123'),
+                'created_at' => $this->today
+            ]
+        );
+        DB::table('model_has_roles')->insert(
+            [
+                'role_id' => $role->id,
+                'model_type' => 'App\Models\user',
+                'model_id' => $id
+            ]
+        );
+    }
+
+    public function referees()
+    {
+        $role = Role::findByName('referee');
+        for($i = 1; $i <= $this->numberOfReferees; $i++){
+            $id = DB::table($this->name)->insertGetId(
+                [
+                    'name' => $this->firstnames[array_rand($this->firstnames)],
+                    'surname' => $this->surnames[array_rand($this->surnames)],
+                    'date_birth' => rand(1989, 2005) . '-' . rand(1, 12) . '-' . rand(1, 28),
+                    'street' => $this->streets[array_rand($this->streets)],
+                    'house_number' => rand(1, 250),
+                    'postal_code' => rand(10, 99) . '-' . rand(100, 999),
+                    'town' => $this->towns[array_rand($this->towns)],
+                    'email' => 'referee'.$i.'@referee.com',
+                    'password' => Hash::make('admin123'),
+                    'created_at' => $this->today
+                ]
+            );
+            DB::table('model_has_roles')->insert(
+                [
+                    'role_id' => $role->id,
+                    'model_type' => 'App\Models\user',
+                    'model_id' => $id
+                ]
+            );
+        }
+    }
+
+    public function coaches()
+    {
+        $role = Role::findByName('coach');
+        for($i = 1; $i <= $this->numberOfCoaches; $i++){
+            $id = DB::table($this->name)->insertGetId(
+                [
+                    'name' => $this->firstnames[array_rand($this->firstnames)],
+                    'surname' => $this->surnames[array_rand($this->surnames)],
+                    'date_birth' => rand(1989, 2005) . '-' . rand(1, 12) . '-' . rand(1, 28),
+                    'street' => $this->streets[array_rand($this->streets)],
+                    'house_number' => rand(1, 250),
+                    'postal_code' => rand(10, 99) . '-' . rand(100, 999),
+                    'town' => $this->towns[array_rand($this->towns)],
+                    'email' => 'coach'.$i.'@coach.com',
+                    'password' => Hash::make('admin123'),
+                    'created_at' => $this->today
+                ]
+            );
+            DB::table('model_has_roles')->insert(
+                [
+                    'role_id' => $role->id,
+                    'model_type' => 'App\Models\user',
+                    'model_id' => $id
+                ]
+            );
+        }
+    }
+
+    public function users()
+    {
+        $role = Role::findByName('user');
+        for($i = 1; $i <= $this->numberOfUsers; $i++){
+            $id = DB::table($this->name)->insertGetId(
+                [
+                    'name' => $this->firstnames[array_rand($this->firstnames)],
+                    'surname' => $this->surnames[array_rand($this->surnames)],
+                    'date_birth' => rand(1989, 2005) . '-' . rand(1, 12) . '-' . rand(1, 28),
+                    'street' => $this->streets[array_rand($this->streets)],
+                    'house_number' => rand(1, 250),
+                    'postal_code' => rand(10, 99) . '-' . rand(100, 999),
+                    'town' => $this->towns[array_rand($this->towns)],
+                    'email' => 'user'.$i.'@user.com',
+                    'password' => Hash::make('admin123'),
+                    'created_at' => $this->today
+                ]
+            );
+            DB::table('model_has_roles')->insert(
+                [
+                    'role_id' => $role->id,
+                    'model_type' => 'App\Models\user',
+                    'model_id' => $id
                 ]
             );
         }
